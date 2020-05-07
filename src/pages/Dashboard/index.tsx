12bf5b32c1847c0d00ed71479193b9476d70d067
Lara <laraamori.machado@gmail.com>
2020-05-07 16:14:21 -0300

    utilizando styled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Title } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return <Title>Explore repositórios no Github</Title>;
 };
 
 export default Dashboard;
